19771a4156bb308ebde3ce1efa34fd2de3da0774
Gytis <gytisbalciunas1551@gmail.com>
2019-04-14 10:59:37 +0300

    update

--- app.js
+++ app.js
@@ -1,47 +1,146 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResults);
+const pensijosKaupimas = document.getElementById("pensijosKaupimas");
+const atlyginimoTipas = document.getElementById("atlyginimo-tipas");
+
+// atlyginimoTipas.addEventListener("change", turnOff);
+
+// // Išjungti pensijos pasirinkimą
+// function turnOff() {
+//   if (atlyginimoTipas.options[atlyginimoTipas.selectedIndex].value == "1") {
+//     pensijosKaupimas.disabled = true;
+//     pensijosKaupimas.value = "0";
+//   } else if (
+//     atlyginimoTipas.options[atlyginimoTipas.selectedIndex].value == "2"
+//   ) {
+//     pensijosKaupimas.disabled = false;
+//   }
+// }
 
 // Calculate Results
-function calculateResults(e){
-  console.log('Calculating...');
+function calculateResults(e) {
+  console.log("Calculating...");
   // UI Vars
-  const amount = document.getElementById('amount');
-  const interest = document.getElementById('interest');
-  const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
+  const atlyginimas = document.getElementById("atlyginimas");
+  const poilsioValandos = document.getElementById("poilsioValandos");
+  const virsvalandziai = document.getElementById("virsvalandziai");
+  // const menuo = document.getElementById("menuo");
+  // const valandosVerte = document.getElementById("valandos-verte");
+  // const poilsioValandosVerte = document.getElementById(
+  //   "poilsio-valandos-verte"
+  // );
+  // const bendraPoilsioVerte = document.getElementById("bendra-poilsio-verte");
+  // const virsvalandziuVerte = document.getElementById("virsvalandziu-verte");
+  // const bendraVirsvalandziuVerte = document.getElementById(
+  //   "bendra-virsvalandziu-verte"
+  // );
+  const ismoketasAtlyginimas = document.getElementById("ismoketas-atlyginimas");
+  const darboDienos = document.getElementById("darbo-dienos");
 
-  const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
+  //Paskaičiuoti valandas iš viso
 
-  // Compute monthly payment
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal*x*calculatedInterest)/(x-1);
+  let valandosIsViso =
+    Number(darboDienos.value) * 8 +
+    Number(poilsioValandos.value) +
+    Number(virsvalandziai.value);
 
-  if(isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+  //Paskaičiuoti kiekvieną vlandų rušį atskirai
+
+  let valandinis =
+    ((atlyginimas.value / (darboDienos.value * 8)) * valandosIsViso) /
+    valandosIsViso;
+
+  let poilsioAtlyginimas = (poilsioValandos.value * valandinis * 2).toFixed(2);
+
+  let virsvalandziuAtlyginimas = (
+    virsvalandziai.value *
+    valandinis *
+    1.5
+  ).toFixed(2);
+
+  //Paskaičiuoti bendrą atlyginimą
+
+  let bendrasAtlyginimas =
+    Number(atlyginimas.value) +
+    Number(poilsioAtlyginimas) +
+    Number(virsvalandziuAtlyginimas);
+
+  // Paskaičiuoti neapmokestinamą pajamų dydi (NPD)
+  let npd = 0;
+  if (bendrasAtlyginimas <= 555) {
+    npd = 300;
+  } else if (300 - 0.15 * (bendrasAtlyginimas - 555) > 0) {
+    npd = 300 - 0.15 * (bendrasAtlyginimas - 555);
   } else {
-    showError('Please check your numbers');
+    npd = 0;
   }
+
+  //Atlyginimas atėmus NPD
+  let atlyginimasPoNpd = 0;
+
+  if (npd <= bendrasAtlyginimas) {
+    atlyginimasPoNpd = bendrasAtlyginimas - npd;
+  } else {
+    atlyginimasPoNpd = bendrasAtlyginimas - bendrasAtlyginimas;
+  }
+
+  //Atlyginimas atėmus Mokesčius
+
+  const gpm = atlyginimasPoNpd * 0.2;
+  const sodra = bendrasAtlyginimas * 0.0698;
+  const vsd = bendrasAtlyginimas * 0.1252;
+
+  atlyginimasPoMokesciu = bendrasAtlyginimas - gpm - sodra - vsd;
+
+  //Atlyginimas atėmus pensija
+
+  let pensija = 0;
+  if (pensijosKaupimas.options[pensijosKaupimas.selectedIndex].value == "1") {
+    pensija = 0.018;
+  } else if (
+    pensijosKaupimas.options[pensijosKaupimas.selectedIndex].value == "2"
+  ) {
+    pensija = 0.03;
+  }
+
+  const galutinisAtlyginimas =
+    atlyginimasPoMokesciu - bendrasAtlyginimas * pensija;
+
+  //Išmokamas altyginimas į rankas
+
+  ismoketasAtlyginimas.value = galutinisAtlyginimas.toFixed(2);
+
+  //Parodyti error jeigu jeigu skaičius yra didesnis arba mažesnis nei nustatytas intervalas
+
+  if (darboDienos.value > 23 || darboDienos.value < 19) {
+    showError(
+      "Darbo dienų skaičius negali buti didesnis nei 23, arba mažesnis nei 19"
+    );
+    ismoketasAtlyginimas.value = "";
+  }
+
+  // if (atlyginimoTipas.options[atlyginimoTipas.selectedIndex].value == "2") {
+
+  // } else {
+
+  // }
 
   e.preventDefault();
 }
 
 // Show Error
-function showError(error){
+function showError(error) {
   // Create a div
-  const errorDiv = document.createElement('div');
+  const errorDiv = document.createElement("div");
 
   // Get elements
-  const card = document.querySelector('.card');
-  const heading = document.querySelector('.heading');
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
 
   // Add class
-  errorDiv.className = 'alert alert-danger';
+  errorDiv.className = "alert alert-danger";
 
   // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
@@ -50,10 +149,10 @@ function showError(error){
   card.insertBefore(errorDiv, heading);
 
   // Clear error after 3 seconds
-  setTimeout(clearError, 3000);
+  setTimeout(clearError, 6000);
 }
 
 // Clear error
-function clearError(){
-  document.querySelector('.alert').remove();
+function clearError() {
+  document.querySelector(".alert").remove();
 }
